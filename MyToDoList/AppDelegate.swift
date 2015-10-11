5403be907b0ad7c480328038f36282a0d12915f8
Yi Qin <yiqin.mems@gmail.com>
2015-10-10 18:00:29 -0700

    Fonts is ready to use.

--- MyToDoList/AppDelegate.swift
+++ MyToDoList/AppDelegate.swift
@@ -17,9 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         
-        
-        
         setupSingletonMethod()
+        setupNavigationBarUI()
         setupViewControllers()
         
         return true
@@ -29,6 +28,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func setupSingletonMethod() {
         CurrentListDataManager.sharedInstance.fakeData()
+    }
+    
+    func setupNavigationBarUI() {
+        // AnyObject
+        let dict: [String: AnyObject] =  [NSForegroundColorAttributeName: UIColor.greenColor() as AnyObject, NSFontAttributeName: UIFont(name: "OpenSans-Bold", size: 13.0) as! AnyObject]
+        
+        UINavigationBar.appearance().titleTextAttributes = dict
     }
     
     // MARK: - ViewControllers in TabBarController
